10a389a10ef96db0c4fe65559a4e4eb23a4e3588
kwdurham <kwd0020@users.noreply.github.com>
2025-12-07 16:02:23 +0000

    Created route wildcard dependant on user ID

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -2,13 +2,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CMS</title>
+    <title>Complaint Management System | Tickets</title>
 </head>
 <body>
-    <h1> ABC Limited Complaint Management System </h1>
+    <h2> All Tickets </h2>
 
-    <a href="/tickets" class="btn">
-        Login
-    </a>
+    <ul> 
+        <li> Tickets Here </li>
+    </ul>
 </body>
 </html>
